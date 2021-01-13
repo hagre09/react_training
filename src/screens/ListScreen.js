69cbf7345c3e003a744f4ca40dc09b609f2e4b04
Emadeldeen Abdallah <emad_566@hotmail.com>
2021-01-13 19:41:20 +0200

    02_03vio.The FlatList Element_Rendering a FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
-    return <Text>ListScreen</Text>;
+    const Frindes = [
+        { name: 'Frinde #1' },
+        { name: 'Frinde #2' },
+        { name: 'Frinde #3' },
+        { name: 'Frinde #4' },
+        { name: 'Frinde #5' },
+        { name: 'Frinde #6' },
+        { name: 'Frinde #7' },
+        { name: 'Frinde #8' },
+        { name: 'Frinde #9' },
+    ];
+    return (
+        <FlatList
+            data={Frindes}
+            renderItem={({item}) =>{
+                return <Text>{item.name}</Text>
+            }}
+        />
+    );
 };
 
 const Styles = StyleSheet.create({});
