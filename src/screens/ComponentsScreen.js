90c117240d28f965389d854e35a5534a623b73ae
Emadeldeen Abdallah <emad_566@hotmail.com>
2021-01-13 13:11:10 +0200

    001. Overview of React Components

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const ComponentsScreen = () => {
+    return <Text style= {Styles.textStyle}>This is the Components Screen</Text>
+};
+
+const Styles = StyleSheet.create({
+    textStyle: {
+        fontSize:30
+    }
+});
+
+export default ComponentsScreen;
