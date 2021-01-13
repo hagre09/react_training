b37a79008d708360e1c1e7c0d140ecd64ae8aa3f
Emadeldeen Abdallah <emad_566@hotmail.com>
2021-01-13 18:53:13 +0200

    01Vio. Building Lists

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ListScreen = () => {
+    return <Text>ListScreen</Text>;
+};
+
+const Styles = StyleSheet.create({});
+
+export default ListScreen;
