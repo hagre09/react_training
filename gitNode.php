9b12674827ae6ed03cedb408624320bd8fd56607
Emadeldeen Abdallah <emad_566@hotmail.com>
2021-01-13 12:01:04 +0200

    first commit without any update

---
+++ gitNode.php
@@ -0,0 +1,26 @@
+==========================================
+# 00  for New porjects
+==========================================
+
+1- For each project create new reposatory
+2- Create ".gitignore" file if it not exists
+3- ".gitignore" file is for to ignore or skip folder or files to no be uploaded
+================================
+
+==========================================
+# 01 Git commands: command Line CMD
+==========================================
+git init
+git remote add origin "reposatory url path"  // To get reposatory from github
+
+git add README.md
+git add .                                    // to add all files to uploud: must be before commit
+git commit -m "commit title"                //creat new commit
+git checkout -b 'new branch name'                     // Create new branch
+git branch                                    // Get current branch name
+git push -u origin "branch name"                  // upload in a branch
+git push -u origin master                   // // push in a master to hold all version of project
+// Go to settings>branches  ans set the master to be the default branch
+
+git branch              //git current branch on local
+git status              //git status on new files on local
