69435727f8455fcb7cc8cf9402e2f4c09f86de89
Emadeldeen Abdallah <emad_566@hotmail.com>
2021-01-13 14:03:42 +0200

    04Vio. Rules of JSX

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text style= {Styles.textStyle}>This is the Components Screen</Text>
+    // const greeting = "Bye thir";
+    const greeting = <Text>Bye thir</Text>;
+
+    return (
+    <View>
+        <Text style= {Styles.textStyle}>This is the Components Screen</Text>
+        {/* <Text>{greeting}</Text> */}
+        {greeting}
+    </View>
+    );
 };
 
 const Styles = StyleSheet.create({
